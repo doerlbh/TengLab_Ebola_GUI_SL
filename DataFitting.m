cde5fb2bb3ae57c62df84de8772a80f5b2291d6d
doerlbh <doerlbh@gmail.com>
2016-04-29 14:22:21 -0700

    plot data

--- DataFitting.m
+++ DataFitting.m
@@ -14,3 +14,23 @@ filename = '/Users/DoerLBH/Dropbox/git/TengLab_Ebola_GUI_SL/Table1_for_code.xlsx
 xlRange = 'A2:D18';
 GUI = xlsread(filename,1,xlRange);
 SL  = xlsread(filename,2,xlRange);
+
+t = 1:17;
+GCase  = GUI(:, 2);
+SCase  =  SL(:, 2);
+GDeath = GUI(:, 3);
+SDeath =  SL(:, 3);
+GCFR   = GUI(:, 4);
+SCFR   =  SL(:, 4);
+
+fig1 = figure;
+plot(t, Gcase,'LineWidth', 2); hold on
+plot(t, Scase,'LineWidth', 2);
+plot(t, GDeath,'LineWidth', 2);
+plot(t, SDeath,'LineWidth', 2);
+title('Case & Death in GUI and SL');
+xlabel('month');
+ylabel('number');
+legend('GCase','SCase', 'GDeath', 'GDeath','Location','northwest');
+
+
