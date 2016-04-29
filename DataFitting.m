5a74cb386ef2cc5e157e0b45645bbcd61b85fac6
doerlbh <doerlbh@gmail.com>
2016-04-28 18:17:52 -0700

    add datafitting

---
+++ DataFitting.m
@@ -0,0 +1,12 @@
+% developed by Baihan Lin
+% Ebola Modeling
+% Data Fitting
+
+% % To Do:
+% 1. Open Excel
+% 2. Read Data
+% 3. Plot Data
+% 4. Fit Model
+% 5. Fit Parameters
+% 6. Plot Model and Data
+
