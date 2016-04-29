b4e26f0523f5e08c25bb5be9804e925731b4c679
doerlbh <doerlbh@gmail.com>
2016-04-29 13:21:01 -0700

    add test data and import excel

--- DataFitting.m
+++ DataFitting.m
@@ -10,3 +10,7 @@
 % 5. Fit Parameters
 % 6. Plot Model and Data
 
+filename = '/Users/DoerLBH/Dropbox/git/TengLab_Ebola_GUI_SL/Table1_for_code.xlsx';
+xlRange = 'A2:D18';
+GUI = xlsread(filename,1,xlRange);
+SL  = xlsread(filename,2,xlRange);
